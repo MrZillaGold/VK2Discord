c2b14fbf2e33ebfa4f08639ac2d1b6496c44c38d
Пронин Егор <mrzillagold@gmail.com>
2021-01-18 17:28:11 +1000

    Добавление недостающих типов

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,13 @@ import { Handler } from "./modules/Handler";
 // @ts-ignore
 import config from "../config.json";
 
+import { Cluster } from "./interfaces";
+
 const { clusters } = config;
 
 console.log("[VK2Discord] Запущен.");
 
-clusters.forEach((cluster, index) =>
+clusters.forEach((cluster: Pick<Cluster, "vk" | "discord">, index: number): void =>
     new Handler({
         ...cluster,
         index: index + 1
