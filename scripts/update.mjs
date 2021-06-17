b494b1f964ee49666adcdbf83c6f831ce0b67ea2
Пронин Егор <mrzillagold@gmail.com>
2021-06-18 02:11:37 +1000

    chore(update): simplify

--- scripts/update.mjs
+++ scripts/update.mjs
@@ -44,36 +44,36 @@ const changes = new Map([
 ]);
 
 if (!clusters || !version_dont_modify_me) {
-    console.warn("[!] Структура вашего конфига больше не поддерживается скриптом, вам необходимо обновить конфиг вручную следуя инструкции.");
-} else {
-    if (version_dont_modify_me >= LATEST_CONFIG_VERSION) {
-        console.log("[!] Текущая версия конфига последняя, обновление не требуется.");
-    } else {
-        config.clusters = clusters.map(({ vk, discord }) => {
-            for (let version = version_dont_modify_me; version !== LATEST_CONFIG_VERSION; version++) {
-                const [vkChanges, discordChanges] = changes.get(version + 1);
-
-                vk = {
-                    ...vk,
-                    ...vkChanges
-                };
-
-                discord = {
-                    ...discord,
-                    ...discordChanges
-                };
-            }
-
-            return {
-                vk,
-                discord
-            }
-        });
-
-        config.version_dont_modify_me = LATEST_CONFIG_VERSION;
-
-        fs.writeFileSync("./config.json", JSON.stringify(config, null, "\t"));
-
-        console.log("[VK2Discord] Конфиг обновлен до последней версии.");
-    }
+    return console.warn("[!] Структура вашего конфига больше не поддерживается скриптом, вам необходимо обновить конфиг вручную следуя инструкции.");
 }
+
+if (version_dont_modify_me >= LATEST_CONFIG_VERSION) {
+    return console.log("[!] Текущая версия конфига последняя, обновление не требуется.");
+}
+
+config.clusters = clusters.map(({ vk, discord }) => {
+    for (let version = version_dont_modify_me; version !== LATEST_CONFIG_VERSION; version++) {
+        const [vkChanges, discordChanges] = changes.get(version + 1);
+
+        vk = {
+            ...vk,
+            ...vkChanges
+        };
+
+        discord = {
+            ...discord,
+            ...discordChanges
+        };
+    }
+
+    return {
+        vk,
+        discord
+    }
+});
+
+config.version_dont_modify_me = LATEST_CONFIG_VERSION;
+
+fs.writeFileSync("./config.json", JSON.stringify(config, null, "\t"));
+
+console.log("[VK2Discord] Конфиг обновлен до последней версии.");
