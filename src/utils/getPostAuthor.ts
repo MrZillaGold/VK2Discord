41ce3358cea192ffe241db7b85481044c1878b2d
Egor <mrzillagold@gmail.com>
2021-10-09 20:05:28 +1000

    chore(getPostAuthor): use different type

--- src/utils/getPostAuthor.ts
+++ src/utils/getPostAuthor.ts
@@ -1,19 +1,19 @@
 import { IWallPostContextPayload } from 'vk-io';
-import { GroupsGroupFull, GroupsProfileItem } from 'vk-io/lib/api/schemas/objects';
+import { GroupsGroupFull, UsersUserFull } from 'vk-io/lib/api/schemas/objects';
 
 export interface IProfile {
     name: string;
     photo_50?: string;
 }
 
-export function getPostAuthor(post: IWallPostContextPayload, profiles: GroupsProfileItem[], groups: GroupsGroupFull[]): IProfile | GroupsGroupFull {
-    const author: (GroupsProfileItem | GroupsGroupFull)[] =
+export function getPostAuthor(post: IWallPostContextPayload, profiles: UsersUserFull[], groups: GroupsGroupFull[]): IProfile | GroupsGroupFull {
+    const author: (UsersUserFull | GroupsGroupFull)[] =
         post.from_id as number > 0 ?
             profiles.filter(({ id }) => id === post.from_id)
             :
             groups.filter(({ id }) => id === Math.abs(post.from_id as number));
 
-    return author.map((profile: GroupsProfileItem | GroupsGroupFull) => {
+    return author.map((profile: UsersUserFull | GroupsGroupFull) => {
         const { name, photo_50, first_name, last_name } = profile;
 
         if (name) {
