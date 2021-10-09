c6f56ac38dfc70d74be3c3cf82d5d8511e4d1975
Egor <mrzillagold@gmail.com>
2021-10-09 18:36:44 +1000

    chore(generateRandomString): rename const

--- src/utils/generateRandomString.ts
+++ src/utils/generateRandomString.ts
@@ -1,11 +1,11 @@
 export function generateRandomString(length: number): string {
     const characters = 'abcdefghijklmnopqrstuvwxyz';
 
-    let id = '';
+    let string = '';
 
     for (let i = 0; i < length; i++) {
-        id += characters.charAt(Math.floor(Math.random() * characters.length));
+        string += characters.charAt(Math.floor(Math.random() * characters.length));
     }
 
-    return id;
+    return string;
 }
