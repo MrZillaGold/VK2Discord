a33551855f2de32835b097bf5f87a3e202184ff5
Pronin Egor <mrzillagold@Gmail.com>
2022-06-27 02:04:17 +0300

    fix: declare clusters type

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,14 @@ const { clusters } = config;
 console.log('[VK2Discord] Запущен.');
 
 const handlers = await Promise.all(
-    clusters.map((cluster, index) => (
-        new Handler({
-            ...cluster,
-            index: index + 1
-        })
-            .init()
-    ))
+    (clusters as Pick<Cluster, 'vk' | 'discord'>[])
+        .map((cluster, index) => (
+            new Handler({
+                ...cluster,
+                index: index + 1
+            })
+                .init()
+        ))
 );
 
 const uniqueHandlers = handlers.reduce<[Storage, Cluster['vk']['group_id'][]][]>((handlers, handler) => {
@@ -35,13 +36,12 @@ const uniqueHandlers = handlers.reduce<[Storage, Cluster['vk']['group_id'][]][]>
 }, []);
 
 uniqueHandlers.forEach(async ([storage, groupIds]) => {
-    const keys = await storage.getKeys();
+    const storageKeys = await storage.getKeys();
 
-    const outdatedKeys = keys.reduce<string[]>((keys, key) => {
-        if (
-            key.startsWith(Storage.PREFIX) &&
-            groupIds.findIndex((id) => key.includes(`-${id}-`)) === -1
-        ) {
+    const outdatedKeys = storageKeys.reduce<string[]>((keys, key) => {
+        const isGroupKeyExist = groupIds.findIndex((id) => key.includes(`-${id}-`)) === -1;
+
+        if (key.startsWith(Storage.PREFIX) && isGroupKeyExist) {
             keys.push(key);
         }
 
